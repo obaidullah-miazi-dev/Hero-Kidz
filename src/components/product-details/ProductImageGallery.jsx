aef326a3864b27123fc6158d137aa91c4ed6f8fa
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2026-01-16 21:03:43 +0600

    style: adjust product image gallery height for better visibility

--- src/components/product-details/ProductImageGallery.jsx
+++ src/components/product-details/ProductImageGallery.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const ProductImageGallery = ({ image, title, discount }) => {
     return (
-        <div className="relative w-full h-full lg:h-full  rounded-2xl overflow-hidden border border-base-300">
+        <div className="relative w-full h-100 lg:h-150  rounded-2xl overflow-hidden border border-base-300">
             <Image
                 src={image}
                 alt={title}
